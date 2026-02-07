398af89d7f9961c0de206cf9a938fe6f13e33266
ajaymagar_45 <ajaymagar812@gmail.com>
2026-02-07 16:56:58 +0530

    Commit_2

---
+++ src/main/java/Loop.java
@@ -0,0 +1,27 @@
+public class Loop {
+    public static void main(String[] args) {
+//Square Pattern program
+//        for (int i=1;i<=10;i++){
+//            for (int j=1;j<=10;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//Right angle triangle
+//          for(int i=1;i<=4;i++){
+//              for(int j=1;j<=i;j++){
+//                  System.out.print("* ");
+//              }
+//              System.out.println();
+//          }
+
+//Inverted rightangel triangel
+         for(int i=4;i>0;i--){
+             for(int j=1;j<=i;j++){
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+    }
+}
